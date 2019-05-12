e67a2a6124d4d2312156be48d163597bf903a825
DanielSchuette <d.schuette@online.de>
2019-05-13 00:55:16 +0200

    fix build process of top-level PID_pendulum module

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 #!/bin/python
 from setuptools import find_packages, setup
 
-setup(name="PID_pendulum", version="0.0.1", packages=find_packages())
+setup(
+    name="PID_pendulum",
+    version="0.0.1",
+    author="Philipp Schuette",
+    author_email="p.schuette@online.e",
+    description="A PID pendulum controller.",
+    url="https://github.com/PhilippSchuette/PID_pendulum",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0 License",
+        "Operating System :: OS Independent",
+    ]
+)
