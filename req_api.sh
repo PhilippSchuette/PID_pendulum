db8717a8c6fe16eb154e08b483a7421c0491c1e6
PhilippSchuette <p.schuette@online.de>
2019-05-13 18:33:21 +0200

    Update req_api.sh to make a valid api request.

--- req_api.sh
+++ req_api.sh
@@ -4,6 +4,6 @@ echo 'Response:'
 if [ "$1" = 'err' ]; then
     curl 'http://localhost:5000/api/v1/?invalid_param'
 else
-    curl 'http://localhost:5000/api/v1/?alpha=0&beta=0&mu=0&phi0=0&phi0_dot=0&max_control=0&frequency=0&deadband=0&set_point=0&precision=0&key=0&N=10'
+    curl 'http://localhost:5000/api/v1/?alpha=4.0&beta=1.5&mu=0.8&phi0=0.25&phi0_dot=0.25&max_control=3.0&frequency=10&deadband=5&set_point=0&precision=5&key=0&N=10'
 fi
 echo
